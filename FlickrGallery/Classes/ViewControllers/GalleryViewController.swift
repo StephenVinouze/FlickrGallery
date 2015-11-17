52627f8a7be588d8326ea2571391b8cd368047a9
Stephen Vinouze <s.vinouze@gmail.com>
2015-11-17 23:07:46 +0100

    [+] Configure Cocoapods with FlickrKit dependency and fetch batch of images

--- FlickrGallery/Classes/ViewControllers/GalleryViewController.swift
+++ FlickrGallery/Classes/ViewControllers/GalleryViewController.swift
@@ -7,26 +7,57 @@
 //
 
 import UIKit
+import FlickrKit
 
 class GalleryViewController : UICollectionViewController {
     
-    var _photos = [UIImage]()
+    private let kFlickrApiKey = "035a49b6782ea1221be5cd6eecca4730"
+    private let kFlickrApiSecret = "5a1cde0b4d0a7c96"
+    private var photos = [NSURL]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for _ in 1...5 {
-            _photos.append(UIImage(named: "Kasual")!)
+        
+        let flickrKit = FlickrKit.sharedFlickrKit()
+        flickrKit.initializeWithAPIKey(kFlickrApiKey, sharedSecret: kFlickrApiSecret)
+        flickrKit.call(FKFlickrInterestingnessGetList()) { (response, error) -> Void in
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                if (response != nil) {
+                    let topPhotos = response["photos"] as! [NSObject: AnyObject]
+                    let photoArray = topPhotos["photo"] as! [[NSObject: AnyObject]]
+                    for photoDictionary in photoArray {
+                        let photoURL = flickrKit.photoURLForSize(FKPhotoSizeSmall240, fromPhotoDictionary: photoDictionary)
+                        self.photos.append(photoURL)
+                    }
+                    self.collectionView?.reloadData()
+                } else {
+                    switch error.code {
+                    case FKFlickrInterestingnessGetListError.ServiceCurrentlyUnavailable.rawValue:
+                        break;
+                    default:
+                        break;
+                    }
+                    
+                    UIAlertView(title: "Error", message: error.localizedDescription, delegate: nil, cancelButtonTitle: "OK").show()
+                }
+            })
         }
     }
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return _photos.count
+        return photos.count
     }
     
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("gallery_cell_identifier", forIndexPath: indexPath) as! GalleryCell
         cell.backgroundColor = UIColor.blackColor()
-        cell.photo.image = _photos[indexPath.row]
+        
+        let urlRequest = NSURLRequest(URL: photos[indexPath.row])
+        NSURLConnection.sendAsynchronousRequest(urlRequest, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
+            cell.photo.image = UIImage(data: data!)
+        })
+        
         return cell
     }
     
---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'FlickrGallery' do
+pod 'FlickrKit'
+end
+
+target 'FlickrGalleryTests' do
+
+end
+
+target 'FlickrGalleryUITests' do
+
+end
+
