5f6ec0330bcdb7afd814a1d19be0cd3b8626c8ff
Stephen Vinouze <s.vinouze@gmail.com>
2015-11-18 12:50:59 +0100

    [+] Add pinch and rotate gestures on images

--- FlickrGallery/Classes/ViewControllers/GalleryViewController.swift
+++ FlickrGallery/Classes/ViewControllers/GalleryViewController.swift
@@ -11,8 +11,9 @@ import FlickrKit
 import SDWebImage
 import MBProgressHUD
 
-class GalleryViewController : UICollectionViewController {
+class GalleryViewController : UICollectionViewController, UICollectionViewDelegateFlowLayout, UIGestureRecognizerDelegate {
     
+    private let photosPerCell = 4
     private let refreshControl = UIRefreshControl()
     private var isLoading = false
     private var photos = [NSURL]()
@@ -27,13 +28,23 @@ class GalleryViewController : UICollectionViewController {
         refreshControl.addTarget(self, action: "onRefresh", forControlEvents: .ValueChanged)
         collectionView?.addSubview(refreshControl)
         
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: "onPinch:")
+        let rotateGesture = UIRotationGestureRecognizer(target: self, action: "onRotate:")
+        
+        pinchGesture.delegate = self
+        rotateGesture.delegate = self
+        
+        collectionView?.collectionViewLayout = PinchLayout()
+        collectionView?.addGestureRecognizer(pinchGesture)
+        collectionView?.addGestureRecognizer(rotateGesture)
+        
         KBLocationProvider.instance().startFetchLocation { (location, error) -> Void in
             dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                if error == nil {
+                if location != nil {
                     self.fetchPhotos(location)
                 }
                 else {
-                    self.showAlertError("Pas de loc")
+                    self.showAlertError(error.localizedDescription)
                 }
             })
         }
@@ -61,7 +72,7 @@ class GalleryViewController : UICollectionViewController {
                         let topPhotos = response["photos"] as! [NSObject: AnyObject]
                         let photoArray = topPhotos["photo"] as! [[NSObject: AnyObject]]
                         for photoDictionary in photoArray {
-                            let photoURL = flickrKit.photoURLForSize(FKPhotoSizeLargeSquare150, fromPhotoDictionary: photoDictionary)
+                            let photoURL = flickrKit.photoURLForSize(FKPhotoSizeSmall240, fromPhotoDictionary: photoDictionary)
                             self.photos.append(photoURL)
                         }
                     }
@@ -87,6 +98,55 @@ class GalleryViewController : UICollectionViewController {
     
     func onRefresh() {
         fetchPhotos(KBLocationProvider.lastLocation())
+    }
+    
+    func onPinch(sender : UIPinchGestureRecognizer) {
+        let pinchLayout = collectionView?.collectionViewLayout as! PinchLayout
+        
+        if sender.state == .Began {
+            let initialPoint = sender.locationInView(collectionView)
+            pinchLayout.pinchedCellPath = collectionView?.indexPathForItemAtPoint(initialPoint)
+        }
+        else if sender.state == .Changed {
+            pinchLayout.pinchedCellScale = sender.scale
+            pinchLayout.pinchedCellCenter = sender.locationInView(collectionView)
+        }
+        else {
+            collectionView?.performBatchUpdates({ () -> Void in
+                pinchLayout.pinchedCellPath = nil;
+                pinchLayout.pinchedCellScale = 1.0;
+                pinchLayout.pinchedCellRotationAngle = 0.0;
+                }, completion: nil)
+        }
+    }
+    
+    func onRotate(sender : UIRotationGestureRecognizer) {
+        let pinchLayout = collectionView?.collectionViewLayout as! PinchLayout
+        
+        if sender.state == .Began {
+            let initialPoint = sender.locationInView(collectionView)
+            pinchLayout.pinchedCellPath = collectionView?.indexPathForItemAtPoint(initialPoint)
+        }
+        else if sender.state == .Changed {
+            pinchLayout.pinchedCellRotationAngle = sender.rotation
+            pinchLayout.pinchedCellCenter = sender.locationInView(collectionView)
+        }
+        else {
+            collectionView?.performBatchUpdates({ () -> Void in
+                pinchLayout.pinchedCellPath = nil;
+                pinchLayout.pinchedCellScale = 1.0;
+                pinchLayout.pinchedCellRotationAngle = 0.0;
+                }, completion: nil)
+        }
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return gestureRecognizer.isKindOfClass(UIPinchGestureRecognizer) && otherGestureRecognizer.isKindOfClass(UIRotationGestureRecognizer)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let photoDimension : CGFloat = (UIDevice.currentDevice().userInterfaceIdiom == .Pad) ? 150 : 100
+        return  CGSizeMake(photoDimension, photoDimension)
     }
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
--- FlickrGallery/FlickrGallery-Bridging-Header.h
+++ FlickrGallery/FlickrGallery-Bridging-Header.h
@@ -3,4 +3,4 @@
 //
 
 #import "KBLocationProvider.h"
-
+#import "PinchLayout.h"
